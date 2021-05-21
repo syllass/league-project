eaadf7ce1c3ee22be762057e6648152c410bcfe0
syllass <aaron_holmgreen@yahoo.com>
2021-05-21 10:50:55 -0500

    youtubeEmbed done

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Youtube from './Youtube';
 
 const Champion = ({ match, imageUrl }) => {
 	const [details, setDetails] = useState({});
+	const [youtubeId, setYoutubeId] = useState('');
 	const champName = match.params.champion;
+
 	useEffect(() => {
 		fetch(
 			`http://ddragon.leagueoflegends.com/cdn/11.10.1/data/en_US/champion/${champName}.json`
 		)
 			.then((res) => res.json())
 			.then((res) => {
-				console.log(res);
+				//console.log(res);
 				setDetails(res.data[match.params.champion]);
 			});
 	}, []);
+
+	useEffect(() => {
+		fetch(
+			`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${champName}%20highlights&key=AIzaSyCNaJHsL2gatyRK2f0L_21VxvJe7AZ0id4`
+		)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res.items);
+				setYoutubeId(res.items[0].id.videoId);
+			});
+	}, []);
+
+	//AIzaSyCNaJHsL2gatyRK2f0L_21VxvJe7AZ0id4
 	return (
 		<div
 			style={{
@@ -42,7 +58,7 @@ const Champion = ({ match, imageUrl }) => {
 			/> */}
 			</div>
 			<p>{details.lore}</p>
-			<p>hi</p>
+			<Youtube embedId={youtubeId} />
 		</div>
 	);
 };
